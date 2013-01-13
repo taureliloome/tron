b1448deb3cd9580042399338415f014376ca951e
Inwhite <inwhite@inwhite-Vostro-V131.(none)>
2013-01-13 16:28:37 +0200

    Pievienoju getUpdateMessage() funkciju, lai sutitu to no servera uz klientu.

--- World.h
+++ World.h
@@ -13,7 +13,8 @@
 
 /* Object type enumerator */
 typedef enum Objects{
-	HEAD=1,
+	EMPTY=0,
+	HEAD,
 	BACK,
 	TAIL,
 	BULLET
@@ -23,10 +24,17 @@ typedef enum Objects{
 typedef struct WorldCell {
 	uint32_t x;
 	uint32_t y;
-    uint32_t type;      //Šūnas tips
-    uint32_t id;	//Spēlētaja id, kuram pieder objekts 
+    	uint32_t type;      //Šūnas tips
+    	uint32_t id;	//Spēlētaja id, kuram pieder objekts 
 	uint32_t dir;
 }__attribute__((packed)) WorldCell_t ;
+
+typedef struct Tail{
+	uint32_t length; //Vienas astes tekošais garums;
+	uint32_t playerId;  //saimnieka ID;
+	struct UpdateTail* cells; //massivs ar astes šūnām;  ja aste dead -1,-1
+	
+}Tail_t;
 
 typedef struct World {
 	void ***Field;	/* Divdimensiju masivs ar noradem uz objektu kurš aizņem konkrētu šunu. */
@@ -34,11 +42,11 @@ typedef struct World {
 	void *Bullets;		/* Visu iespējamo ložu pūls. */
 
 	void *Bikes;		/* Spēlētāju motocikli */
-	void **Tails;		/* Spēlētāju astes */
+	Tail_t *Tails;		/* Spēlētāju astes */
 
 	/* Pasaules configurācija */
    	uint32_t height;    
-    uint32_t width;
+    	uint32_t width;
 	uint32_t timeout;
 	uint32_t frameRate;
 	uint32_t bulletSpeed;
@@ -49,12 +57,124 @@ typedef struct World {
 	uint32_t bulletCountMax;
 	uint32_t bulletCountAlive;
 	uint32_t playerCountAlive;
+	uint32_t tailCountAlive;
 }World_t;
 
 struct UpdatePlayer* getSelf(World_t *MyWorld)
 {
-	return MyWorld->Players;
+	return MyWorld->Players;   // Jo massiva [0] elements ir vienads ar pointera adresi
 }
+
+
+void * getUpdateMessage(World_t *someWorld, size_t *length)
+{
+	void * packet,*tailPacket;
+	struct Header  header;
+	struct UpdatePlayerHeader plHeader;
+	struct UpdatePlayer *tempPlayer, *playerList;
+
+	struct UpdateBulletHeader blHeader;
+	struct UpdateBullet *tempBullet,*bulletList;
+	
+	struct UpdateTotalTailHeader totalTailHeader;  //Visu astes galvene
+	struct UpdateTailHeader *tempTailHeader; //Vienas astes galvene
+	struct UpdateTail tempCell,*tempCellList; // veina astes šūna;  šūnu saraksts
+	Tail_t *tempTail;  // lai ielasītu no pasaules vieni asti
+
+	int i=0,k=0,cellLengthSumm=0,size=0;
+
+	/* Creating packet with list of players */
+
+	plHeader.playerCount = someWorld->playerCountAlive;
+	size += sizeof(&plHeader);	
+
+	playerList = ( struct UpdatePlayer* ) malloc( someWorld->playerCountAlive * sizeof( struct UpdatePlayer ) ); // Izdalam atminu speletaju sarakstam
+	size += someWorld->playerCountAlive * sizeof( struct UpdatePlayer );	
+
+	tempPlayer = (struct UpdatePlayer* ) someWorld->Players;
+
+	for(i = 0; i < someWorld->playerCountAlive; i++)
+	{
+		memcpy(playerList, tempPlayer, sizeof(tempPlayer));  // ierakstam sarakstaa pa speletajam
+		playerList += sizeof( struct UpdatePlayer);
+		tempPlayer += sizeof( struct UpdatePlayer);
+	}
+	
+	/* Creating packet with list of bullets */
+
+	blHeader.bulletCount = someWorld->bulletCountAlive;
+	size += sizeof(&blHeader);
+
+	bulletList = (struct UpdateBullet* ) malloc( someWorld->bulletCountAlive * sizeof( struct UpdateBullet)); // Izdalam atminu lozu sarakstam
+	size += someWorld->bulletCountAlive * sizeof( struct UpdateBullet );
+
+	tempBullet = (struct UpdateBullet* ) someWorld->Bullets;
+
+	for(i = 0; i < someWorld->bulletCountAlive; i++)
+	{
+		memcpy( bulletList, tempBullet, sizeof( tempBullet ) );  // ierakstam sarakstaa pa lodei
+		bulletList += sizeof( struct UpdateBullet );
+		tempBullet += sizeof( struct UpdateBullet );
+	}
+	
+	/* Creating packet with list of tail */
+	
+	totalTailHeader.totalTailLength = someWorld->tailCountAlive;	
+	tempTail = &someWorld->Tails[0];
+
+	for(i = 0; i < someWorld->tailCountAlive; i++)
+	{
+		cellLengthSumm += someWorld->Tails[i].length;
+	}
+
+	tailPacket = malloc( sizeof(struct UpdateTail) * cellLengthSumm + sizeof(struct UpdateTailHeader) * someWorld->tailCountAlive );
+	size += sizeof( struct UpdateTail ) * cellLengthSumm + sizeof( struct UpdateTailHeader) * someWorld->tailCountAlive;
+	for(i = 0; i < someWorld->tailCountAlive; i++)
+	{
+		tempTailHeader = (struct UpdateTailHeader *) malloc( sizeof( struct UpdateTailHeader) ); // Izdalam atminu astes galvenei
+		tempTailHeader->id = tempTail->playerId;
+		tempTailHeader->tailCount = tempTail->length;
+		
+		
+		tempCellList = (struct UpdateTail *) malloc( tempTail->length * sizeof( struct UpdateTail) );
+		for(k=0 ; k < tempTail->length ; k++)
+		{
+			memcpy(tempCellList, &tempTail->cells[k], sizeof(tempTail->cells[k]));  // ierakstam sarakstaa pa astes šunai
+			tempCellList += sizeof(struct UpdateTail);
+		}
+		tempTail += sizeof(Tail_t);
+		memcpy(tailPacket, tempTailHeader, sizeof(tempTailHeader));		
+		tailPacket += sizeof(struct UpdateTailHeader);
+		memcpy(tailPacket, tempCellList, sizeof(tempCellList) );
+		tailPacket += sizeof(tempCellList);
+	}
+
+	/* Calculating length of final packet */
+
+	packet = malloc( size );
+	
+	memcpy(packet, &plHeader, sizeof( &plHeader) );
+	packet+=sizeof(&plHeader);
+
+	memcpy( packet, playerList, sizeof(playerList) );
+	packet += sizeof( playerList );
+	
+	memcpy( packet, &blHeader, sizeof(&blHeader) );
+	packet += sizeof(&blHeader);
+
+	memcpy( packet , bulletList, sizeof(bulletList) );
+	packet += sizeof( bulletList );
+	
+	memcpy( packet , &totalTailHeader, sizeof(&totalTailHeader) );
+	packet += sizeof(&totalTailHeader);
+
+	memcpy(packet, tailPacket, sizeof(tailPacket) );
+	size += sizeof( tailPacket );
+
+	*length = sizeof( packet);
+	return packet;
+}
+
 #if 0
 void CreateServerWorld(World *someWorld)
 {
@@ -62,8 +182,8 @@ void CreateServerWorld(World *someWorld)
 	MyWorld=someWorld;
 	
 	MyWorld->Bikes;
-    MyWorld->height=80;           
-    MyWorld->width=20;            
+    	MyWorld->height=80;           
+    	MyWorld->width=20;            
 	MyWorld->tailLength=10;
 	MyWorld->frameRate=60;
 	MyWorld->bulletSpeed=2;
